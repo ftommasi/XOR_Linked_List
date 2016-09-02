8101a9038c6ce1001649c1c88b2cffb656d524bb
Fausto G. Tommasi <mmasito123@yahoo.com>
2016-09-01 19:07:30 -0500

    implemented insert - not functional

--- xorlist.cpp
+++ xorlist.cpp
@@ -1,47 +1,6 @@
 #include <iostream>
+#include <stdint.h>
 
-class xorlist{
-
-public:
-  
-  int size;
-  
-
-private:
-  
-  node* head;
-  node* tail;
-
-};
-
-
-void xorlist::insert(int val){
-  //initial values
-  node* current = head;
-  node* prev = 0; //null == 0
-  node* next = head->np; //np ^ 0 == np
-
-  while(current != tail){
-    
-  }
-  
-  node* new_node = new node();
-  new_node->np = prev;
-  new_node->elem = val;
-  tail=new_node;
-
-
-}
-
-void xorlist::dump(){
-  cout << "]|-- ";
-  node* current = head;
-  while(current != tail){
-    cout << current -> elem << " -> ";
-  }
-}
-
-//template<typename T>
 struct node{
   
   //Debug pointes
@@ -53,11 +12,77 @@ struct node{
 
 };
 
+void dump_pointerrs(node* a, node* b, node* c){
+//  std::cout << ""
+}
 
+class xorlist{
+
+public:
+  
+  int size;
+  
+  void insert(int val){
+    //initial values
+    node* current = head;
+    node* prev = 0; //null == 0
+    node* next = head->np; //np ^ 0 == np
+    
+    while(current != tail){
+      std::cout << "current: " << current << std::endl;    
+      std::cout << "prev: " << prev << std::endl;    
+      std::cout << "next: " << next << std::endl;
+      std::cout << "tail: " << tail << std::endl;
+      node* old = current;
+      current = next;
+      if(current == tail)
+        break;
+
+      uintptr_t u_next = (uintptr_t)old->np ^ (uintptr_t)current;
+      uintptr_t u_np = (uintptr_t)old->np ^ (uintptr_t)next;
+      next = (node*)next;
+      prev = old;
+    }
+  
+    node* new_node = new node();
+    new_node->np = prev;
+    new_node->elem = val;
+    tail=new_node;
+
+
+  }
+
+  void dump(){
+    std::cout << "]|-- ";
+    node* current = head;
+    while(current != tail){
+      std::cout << current -> elem << " -> ";
+      node* old = current;
+      current = (node*)( (uintptr_t)old ^ (uintptr_t)current);
+    }
+  }
+
+ 
+
+private:
+  
+  node* head;
+  node* tail;
+
+};
+
+
+//template<typename T>
 
 int main(){
- 
-  //Test  
+  //Test 
+  xorlist list;
+  for(int i=0; i<4; i++){
+    list.insert(i);
+  }
+
+  std::cout << "done inserting" << std::endl;
+  list.dump();
 
 
 }
